45a801521f567a4a949d41b69a9c4c6ec077674d
SwapnilChatakwar17 <swapnilchatakwar1997@gmail.com>
2022-04-28 17:29:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Resturant from "./components/Basics/Resturant";
+// import UseState from "./components/Hooks/useState";
+// import UseEffect from "./components/Hooks/useEffect";
+// import UseReducer from "./components/Hooks/useReducer";
+
+const App = () => {
+  return (
+  <Resturant />
+  // <UseState />
+  // <UseEffect />
+  // <UseReducer />
+  );
+};
+
+export default App;
---
+++ src/components/covid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Covid = () => {
+
+    useE
+  return (
+    <div>
+        <h1> 🔴 LIVE</h1>
+        <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+
+    </div>
+  )
+}
+
+export default Covid
